1d8166635f340c3153bfff1536082bf76cddcccc
kreolwolf1 <alexandr.shilo@nasctech.com>
2012-05-25 20:23:40 +0300

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PizzaHelios::Application.config.secret_token = 'c04f960a3b32c6c9343797352b0854cb74ab85294051d4a9519fa2f4203ae360dd48f9567d92b2471d00350e81b96db120d106c4fa644fe99a91c138886b51d3'
