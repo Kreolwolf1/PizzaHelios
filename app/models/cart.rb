65d3bd622285bd02dbb03a77a06aa25387c84dcc
kreolwolf1 <alexandr.shilo@nasctech.com>
2012-05-27 23:28:24 +0300

    add total method

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,15 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
+
+	def total_price
+		if line_items.present?
+			
+			@total = line_items.map {|item|
+				item.count * item.pizza.cost.to_f 
+			}
+			return @total.sum.round(2)
+		end
+		
+	end
+
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,11 @@ class LineItem < ActiveRecord::Base
 	belongs_to :pizza
 	belongs_to :car
 
+
+	def total_price
+	  if pizza.present? 	
+		(pizza.cost.to_f * count).round(2) 
+	  end
+	end
+
 end
